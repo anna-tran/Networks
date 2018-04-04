ac62c7a6d64481c561d64273ce9719707dcefa8c
Anna Tran <anna@Annas-MacBook-Pro.local>
2018-04-04 17:14:18 -0600

    finished readme for adaptive tree walk

--- AdaptiveTreeWalk/MACProtocol.cpp
+++ AdaptiveTreeWalk/MACProtocol.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>       /* time */
 #include <string>
 #include <map>
 #include <queue>
@@ -14,15 +15,21 @@
 
 using namespace std;
 
-
+/*
+	Represents the information of success, collision and idle probe rates.
+*/
 struct ProbeRate {
 	float success;
 	float collision;
 	float idle;
 	float successRate;
+	float collisionRate;
+	float idleRate;
 };
 
-
+/*
+	Initiate map of level to ProbeRate, with zeros in all fields.
+*/
 void initMapWithI(int* is, int isLen, map<int,ProbeRate>* iCount) {
 	for (int j = 0; j < isLen; j++) {
 		int i = *(is+j);
@@ -31,10 +38,15 @@ void initMapWithI(int* is, int isLen, map<int,ProbeRate>* iCount) {
 		pr.collision = 0;
 		pr.idle = 0;
 		pr.successRate = 0;
+		pr.collisionRate = 0;
+		pr.idleRate = 0;
 		(*iCount)[i] = pr;
 	}
 }
 
+/*
+	Create a random set of exactly 'numActive' active nodes
+*/
 void generateRandCase(int* nodes, int numStations, int numActive){
 	set<int> stationSet;
 	// get random list of nodes
@@ -48,18 +60,19 @@ void generateRandCase(int* nodes, int numStations, int numActive){
 		nodes[randInt] = 1;
 	}
 
-	// for (int i = 0; i < NUM_STATIONS; i++) {
-	// 	printf("%d ", *(nodes+i));
-	// }
-	// printf("\n");
 }
 
+/*
+	Recalculate number of sections at the new level, and the number of nodes per section.
+*/
 void getSectionInfo(int power, int* numSections, int* sizeSection, int sizeNodes) {
 	(*numSections) = (int) pow(2,power);
-	// printf("power of %d\n",power);
 	(*sizeSection) = sizeNodes/(*numSections);
 }
 
+/*
+	Scan the section for active/ready nodes and return the number of active nodes (max 2).
+*/
 int scanSection(int* nodes, int sizeNodes, int start, int end) {
 	int numActive = 0;
 	if (start < 0 || end > sizeNodes) {
@@ -71,21 +84,37 @@ int scanSection(int* nodes, int sizeNodes, int start, int end) {
 		if (*(nodes+i) == 1) {
 			numActive++;
 		}
+		// stop scanning once you know that there's more than one active node
+		if (numActive > 1) {
+			i = end;
+		}
 	}
 	return numActive;
 
 }
 
+/*
+	Check if there's a collision using the number of active child nodes 
+	from the left and right sides
+*/
 bool hasCollision(int numActiveLeft, int numActiveRight) {
 	return (numActiveLeft > 0 && numActiveRight > 0)
 			|| (numActiveLeft > 1 || numActiveRight > 1);
 }
 
+/*
+	Check if there's a success using the number of active child nodes 
+	from the left and right sides
+*/
 bool hasSuccess(int numActiveLeft, int numActiveRight) {
 	return ((numActiveLeft == 1 && numActiveRight == 0)
 			|| (numActiveLeft == 0 && numActiveRight == 1));
 }
 
+/*
+	Initiate the queue of nodes to probe with all nodes defined by the number
+	of sections and the size of each section
+*/
 void initQueue(queue< pair<int,int> >* q, int numSections, int sizeSection) {
 	int start, end;
 	for (int i = 0; i < numSections; i++) {
@@ -94,24 +123,34 @@ void initQueue(queue< pair<int,int> >* q, int numSections, int sizeSection) {
 		pair<int,int> p (start,end);
 		q->push(p);
 	}	
-	// printf("init queue has %d sections\n",numSections);
 }
 
+
+/*
+	Push a node onto the queue if there are multiple active child nodes on either side of
+	the current node
+*/
 void pushIfMultActive(queue< pair<int,int> >* q, 
 						pair<int,int>* leftSection, pair<int,int>* rightSection, 
 						int numActiveLeft, int numActiveRight) {
-	// active on both sides
+	// >1 active nodes on both sides
 	if (numActiveLeft > 0 && numActiveRight > 0) {
 		q->push(*leftSection);
 		q->push(*rightSection);
+	// >1 active nodes on the left side
 	} else if (numActiveLeft > 1) {
 		q->push(*leftSection);
+	// >1 active nodes on the right side
 	} else if (numActiveRight > 1) {
 		q->push(*rightSection);
 	}
 
 }
 
+/*
+	Probe nodes at level i if to determine how many probes needed to allow all
+	nodes to send their information
+*/
 void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) {
 	// number of sections to check initially 2^level
 	int power = level, numSections, sizeSection;
@@ -125,9 +164,7 @@ void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) 
 	ProbeRate* pr = &(iCount->at(level));
 	// int i = 8;
 	while (!q.empty()) {
-		// printf("size of queue %lu\n",q.size());
-		// TODO
-		// check all left and check all right
+		// get the node to look at
 		pair<int,int> p = q.front();
 		q.pop();
 		start = p.first;
@@ -137,9 +174,6 @@ void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) 
 			power++;
 			getSectionInfo(power, &numSections, &sizeSection, sizeNodes);
 		}		
-		// printf("looking at start: %d end: %d\n", start,end);
-		// printf("size section = %d\n", sizeSection);
-		// printf("num sections = %d\n", numSections);
 		
 
 		int childSectionSize = sizeSection/2;
@@ -147,7 +181,6 @@ void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) 
 		int e1 = start+childSectionSize;
 		int s2 = start+childSectionSize;
 		int e2 = end;
-		// printf("s1 %d e1 %d s2 %d e2 %d\n",s1,e1,s2,e2);
 
 		int numActiveLeft = scanSection(nodes, sizeNodes, s1, e1);
 		int numActiveRight = scanSection(nodes, sizeNodes, s2, e2);
@@ -156,7 +189,6 @@ void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) 
 		if (hasCollision(numActiveLeft, numActiveRight)) {
 			pr->collision++;
 			// get new start end indices and add to queue
-			
 			pair<int,int> pleft (s1,e1);
 			pair<int,int> pright (s2,e2);
 			pushIfMultActive(&q, &pleft, &pright, numActiveLeft, numActiveRight);
@@ -170,33 +202,47 @@ void probeAtI(int level, int* nodes, int sizeNodes, map<int,ProbeRate>* iCount) 
 
 }
 
-// 
-void calcSuccessRate(map<int,ProbeRate>* iCount) {
+/*
+	Calculate the success, collision and idle rates for all levels in the map
+*/ 
+void calcProbeRates(map<int,ProbeRate>* iCount) {
 	for (auto it = iCount->begin(); it != iCount->end(); it++) {
 		ProbeRate* pr = &(it->second);
-		// printf("\nbefore dividing\ni: %d\tcol: %.0f suc: %.0f idle: %.0f\n",it->first,pr->collision,pr->success,pr->idle);
 		float totalProbes = pr->collision + pr->success + pr->idle;
 		pr->successRate = pr->success/totalProbes;
+		pr->collisionRate = pr->collision/totalProbes;
+		pr->idleRate = pr->idle/totalProbes;
 	}
 }
 
+
+/*
+	Convert a float to a string, to two decimal places
+*/
 string floatToStr(float f) {
 	string s(16, '\0');
-	auto written = snprintf(&s[0], s.size(), "%.5f", f);
+	auto written = snprintf(&s[0], s.size(), "%.2f", f);
 	s.resize(written);
 	return s;
 }
 
+/*
+	Append the output to an output file stream.
+	The output consists of all the probe rates for each level, for a given
+	k number of active nodes.
+*/
 void appendToOutput(ofstream* ofs, map<int,ProbeRate>* iCount, int k){ 
 	string output;
 	output.append("For " + to_string(k) + " random active stations:\n");
-	output.append("Level\tSuccess Rate\n");
+	output.append("Level\tSuccess\tCollision\tIdle\n");
 
 	for (auto it = iCount->begin(); it != iCount->end(); it++) {
 		int level = it->first;
 		ProbeRate* pr = &(it->second);
 		output += to_string(level) 
-			+ "\t" + (to_string(pr->successRate)) 
+			+ "\t" + (floatToStr(100 * pr->successRate)) 
+			+ "\t" + (floatToStr(100 * pr->collisionRate)) 
+			+ "\t" + (floatToStr(100 * pr->idleRate)) 
 			+ "\n"; 
 
 	}
@@ -206,6 +252,8 @@ void appendToOutput(ofstream* ofs, map<int,ProbeRate>* iCount, int k){
 }
 
 int main(int argc,char** argv) {
+	// randomize the seed for RNG
+	srand (time(NULL));
 	ofstream ofs ("TreeWalk.txt", ofstream::out);
 
 	// ks	number of stations ready to send
@@ -215,7 +263,7 @@ int main(int argc,char** argv) {
 	int ks [11] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
 	int is [6] = {0, 2, 4, 6, 8, 10};
 
-	
+
 	int nodes[NUM_STATIONS];
 	string output;
 	// simulate for every value of k
@@ -225,47 +273,24 @@ int main(int argc,char** argv) {
 		// value 	number of probes
 		map<int,ProbeRate> iCount;
 		initMapWithI(is,numI,&iCount);
-		// printf("finished init\n");
 		// generate 100 test cases
 		for (int j = 0; j < NUM_TESTS; j++) {
 			memset(&nodes,0,sizeof(nodes));
 			generateRandCase(nodes, NUM_STATIONS, k);
 
 			// simulate for each level i
-			for (size_t ii = 0; ii < numI; ii++) {
-
+			for (int ii = 0; ii < numI; ii++) {
 				int i = is[ii];
 				probeAtI(i, nodes, NUM_STATIONS, &iCount);	
 			}
 			
 		}
 		
-		calcSuccessRate(&iCount);
+		calcProbeRates(&iCount);
 		appendToOutput(&ofs, &iCount, k);
 	}
 
-	// write this to file later
-	// printf("%s\n", output.c_str());
 	ofs.close();
 	return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ AdaptiveTreeWalk/readme.md
@@ -0,0 +1,22 @@
+# Adaptive Tree Walk MAC Protocol
+
+This program simulates the average case performance of the Adaptive Tree Walk MAC Protocol on a set of 2^10 = 1024 stations. It generates 100 test cases in which there are k stations ready to send data frames, where k = 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, and 1024. For each value of k, it probes starting at level i if there is a collision between nodes trying to send data simulataneously and moves downwards for every collision. These levels are i = 0, 2, 4, 6, 8, and 10.
+
+ Stations generate frames randomly and independently from each other, and each station is equally likely to generate a frame in each round. 
+
+## Compilation
+
+To compile this program,
+
+    g++ MACProtocol.cpp
+
+To run this program,
+
+    ./a.out
+
+Running the program will produce a file called TreeWalk.txt containing the average success, collision and probe rates for each call of the simulation. These numbers will vary for each run of the program since the random number generator seed is based on the current calendar time.
+
+## Results and Observations
+
+The successful probe rates are graphed and all probe rates are visualized in
+a table in the 'TreeWalk Results' PDF file. Observations concerning the results are also noted in this document.
